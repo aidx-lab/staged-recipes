766c0af79b068444a8d621b1b04f3e3cbcc85b2c
Dominik Winkelbauer <dominik.winkelbauer@tum.de>
2026-07-13 14:41:43 +0200

    Adds pragma removal workaround

--- recipes/racket/build.sh
+++ recipes/racket/build.sh
@@ -4,7 +4,14 @@
 # is in the current source directory.
 # Packages can be added once the package manager is configured
 # to work for this particular version number.
+cat > "$SRC_DIR/cpp-wrapper.sh" << 'EOF'
+#!/bin/bash
+x86_64-conda-linux-gnu-cpp "$@" | grep -v '^#pragma GCC diagnostic'
+EOF
+chmod +x "$SRC_DIR/cpp-wrapper.sh"
+
 make unix-style CPUS="$CPU_COUNT" PREFIX="$PREFIX" PKGS="" \
+  CPP="$SRC_DIR/cpp-wrapper.sh" \
   CFLAGS="${CFLAGS} -Wno-implicit-function-declaration -Wno-incompatible-pointer-types -Wno-implicit-int -Wno-return-local-addr -std=gnu17"
 
 # Set up the package manager.
